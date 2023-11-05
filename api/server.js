fad5c2a6847eee4ea30d401539ca2a567a8b02bf
david <davidlaranjeiracosta@gmail.com>
2023-11-05 22:18:34 +0000

    fix

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://127.0.0.1:8081" || "http://localhost:8081"
 };
 
 const db = require("./app/models");
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 require("./app/routes/tutorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 8080 || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
